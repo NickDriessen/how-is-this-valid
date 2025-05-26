3f4fc32376aeb9c602dca0d2368704b939270509
Nick Driessen <nick.driessen@hotmail.com>
2025-05-26 13:51:20 +0200

    changed 2 numbers so its a bit more logical(in printf)

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(void)
     int array[5] = {13, 12, 19, 420, 69};
 
     printf("%d\n", array[0]);
-    printf("%d\n", 2[array]);
-    printf("%d\n", *(array + 1));
+    printf("%d\n", 1[array]);
+    printf("%d\n", *(array + 2));
     printf("%d\n", *(array + 1 + 2));
     printf("%d\n", *(array + 2 * 2));
 
